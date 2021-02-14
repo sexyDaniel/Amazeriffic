37db774276df0337353dc0ee17e2ff9b9bbb086b
Danil <danil.lazarev.90@mail.ru>
2021-02-15 01:20:30 +0300

    Update app.js with cicle for

--- scripts/app.js
+++ scripts/app.js
@@ -1,21 +1,10 @@
 var main = function(){
-    var tabActive = function(num){
-        $(".tabs span").removeClass("active");
-        $(".tabs a:nth-child("+num+") span").addClass("active");
-        $("main .content").empty();
+    for(tabActive = 1;tabActive<=3;tabActive++){
+        $(".tabs a:nth-child("+tabActive+") span").on("click",function(){
+            $(".tabs span").removeClass("active");
+            $(this).addClass("active");
+            return false;
+        });
     }
-    $(".tabs a:nth-child(1)").on("click",function(){
-        tabActive(1);
-        return false;
-    });
-    $(".tabs a:nth-child(2)").on("click",function(){
-        tabActive(2);
-        return false;
-    });
-    $(".tabs a:nth-child(3)").on("click",function(){
-        tabActive(3);
-        return false;
-    });
-    
 }
 $(document).ready(main);
