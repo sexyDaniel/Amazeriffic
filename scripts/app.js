99e52f92a01b4b4291a9bdc4ac77a60965c8d94a
Danil <danil.lazarev.90@mail.ru>
2021-02-15 16:25:54 +0300

    Update app.js(add output for second tab)

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,29 @@
 var main = function(){
+    var toDos = ["Прочитать книгу",
+    "Купить молоко",
+    "Написать курсач",
+    "Сдать лабу",
+    "Посмотреть лекцию",
+    "Пройти собеседование",
+    "Сделать домашку"]
     $(".tabs a span").toArray().forEach(function(element){
         $(element).on("click",function(){
+            var $element = $(element)
+            var $content;
             $(".tabs a span").removeClass("active");
-            $(element).addClass("active");
+            $element.addClass("active");
             $("main .content").empty();
+            if($element.parent().is(":nth-child(1)")){
+                console.log("1")
+            } else if ($element.parent().is(":nth-child(2)")){
+                $content = $("<ul>")
+                toDos.forEach(function(todo){
+                    $content.append($("<li>").text(todo))
+                })
+                $("main .content").append($content);
+            } else if ($element.parent().is(":nth-child(3)")){
+                console.log("3")
+            } 
             return false;
         })
     })
