76d237da0db8fb0395c7ab6cb3625b722497344c
Danil <danil.lazarev.90@mail.ru>
2021-02-24 21:10:06 +0300

    update tags in app.js

--- scripts/app.js
+++ scripts/app.js
@@ -21,7 +21,20 @@ var main = function(toDosObj){
                     $content.append($("<li>").text(todo))
                 })
                 $("main .content").append($content);
-            } else if ($element.parent().is(":nth-child(3)")){
+            }else if ($element.parent().is(":nth-child(3)")){
+                console.log("Tegs click")
+                var organizedByTag = organizeByTag(toDosObj)
+                organizedByTag.forEach(function(tag){
+                    var $tagName = $("<h3>").text(tag.name)
+                    $content = $("<ul>")
+                    tag.toDos.forEach(function(description){
+                        var $li = $("<li>").text(description)
+                        $content.append($li)
+                    })
+                    $("main .content").append($tagName)
+                    $("main .content").append($content)
+                })
+            } else if ($element.parent().is(":nth-child(4)")){
                 $content = $('<textarea type="text" class="newQuestion" placeholder="Введите новую задачу"/>'+
 				'<button class="addBtn">+</button>')
                 $("main .content").append($content);
