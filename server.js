dd8178c0978618f08ed5a8c61baf2c4639dcd2ef
Danil <danil.lazarev.90@mail.ru>
2021-03-02 15:25:38 +0300

    Finish Pract 7

--- server.js
+++ server.js
@@ -1,49 +1,37 @@
 var express = require("express"),
-http = require("http"),
-app = express(),
-toDos = [
-    {
-        "description" : "Прочитать книгу",
-        "tags" : [ "Книги", "чтение" ]
-    },
-    {
-        "description" : "Купить молоко",
-        "tags" : [ "покупки", "магазин" ]
-    },
-    {
-        "description" : "Написать курсач",
-        "tags" : [ "учеба", "обязательно" ]
-    },
-    {
-        "description" : "Сдать лабу",
-        "tags" : [ "учеба" ]
-    },
-    {
-        "description" : "Посмотреть лекцию",
-        "tags" : [ "учеба", "питомцы" ]
-    },
-    {
-        "description" : "Пройти собеседование",
-        "tags" : [ "работа", "полезно" ]
-    },
-    {
-        "description" : "Сделать домашку",
-        "tags" : [ "учеба" ]
-    },
-    {
-        "description" : "Сделать ",
-        "tags" : [ "bfbdbvf" ]
-    }
-];
+	http = require("http"),
+	mongoose = require("mongoose"),
+	app = express(); 
 app.use(express.static(__dirname + "/client"));
+var ToDoSchema = mongoose.Schema({
+    description: String,
+    tags: [ String ]
+});
+var ToDo = mongoose.model("ToDo", ToDoSchema);
 http.createServer(app).listen(3000);
 app.get("/toDosTags.json", function (req, res) {
-    res.json(toDos);
+    ToDo.find({}, function (err, toDos) {
+        console.log(toDos)
+        res.json(toDos);
+    });
 });
 app.use(express.urlencoded());
+mongoose.connect('mongodb://localhost/amazerific', {useNewUrlParser: true,useUnifiedTopology: true })
 app.post("/todos", function (req, res) {
-    var newToDo = req.body;
-    console.log(newToDo);
-    toDos.push(newToDo);
-    res.json({"message":"Вы размещаетесь на сервере!"});
+	console.log(req.body);
+	var newToDo = new ToDo({"description":req.body.description,
+		"tags":req.body.tags});
+	newToDo.save(function (err, result) {
+		if (err !== null) {
+			console.log(err);
+			res.send("ERROR");
+		} else {
+			ToDo.find({}, function (err, result) {
+				if (err !== null) {
+					res.send("ERROR");
+				}
+				res.json(result);
+			});
+		}
+	});
 });
